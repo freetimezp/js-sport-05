ac43f41825477c8908810d4d0ce36b9e5f6452d8
freetimezp <kraftukrnet@ukr.net>
2023-08-14 23:03:24 +0300

    03. js-sport-05. Banner section. Show, hide menu.

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+function toggleNav() {
+    const nav = document.querySelector('.nav');
+    const menu = document.querySelector('.menu');
+
+    nav.classList.toggle('active');
+    menu.classList.toggle('active');
+}
