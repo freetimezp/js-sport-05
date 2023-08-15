5beb033dc9821368601a2e40dec1da067a1c0491
freetimezp <kraftukrnet@ukr.net>
2023-08-15 22:26:20 +0300

    07. js-sport-05. Banner section. Video.

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,17 @@ function toggleNav() {
     nav.classList.toggle('active');
     menu.classList.toggle('active');
 }
+
+function toggleVideo() {
+    const video = document.querySelector('.bg-video');
+    const play = document.querySelector('.play');
+    const slider = document.querySelector('.swiper');
+    const content = document.querySelector('.content');
+
+    video.classList.toggle('active');
+    play.classList.toggle('active');
+    slider.classList.toggle('active');
+    content.classList.toggle('active');
+}
+
+
